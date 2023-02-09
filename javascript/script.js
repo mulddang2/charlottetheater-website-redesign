9781e635c3db29ab2bf145fe4f3b97eb9971718c
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2023-02-09 22:36:27 +0900

    [mobile] section-cast
    - 캐스팅 섹션, 접근성 태그추가

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,45 @@
-const swiper = new Swiper('.swiper', {
-    effect: 'cards'
+let thisSlide,
+    slidesLength;
+
+swiperNotice = new Swiper('.swiper.notice', {
+    effect: 'cards',
+    a11y: {
+        prevSlideMessage: '이전 슬라이드',
+        nextSlideMessage: '다음 슬라이드',
+        slideLabelMessage: '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
+    },
+});
+swiperCast = new Swiper('.swiper.cast', {
+    autoplay: {
+        delay: 1500,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    a11y: {
+        prevSlideMessage: '이전 슬라이드',
+        nextSlideMessage: '다음 슬라이드',
+        slideLabelMessage: '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
+    },
+    on: {
+        init: function () {
+            thisSlide = this;
+            autoPlayBtn = document.querySelector('.wrap-autoplay-control > button');
+            autoPlayBtn.addEventListener('click', (e) => {
+                autoPlayState = autoPlayBtn.getAttribute('aria-pressed');
+                if (autoPlayState === 'false') {
+                    autoPlayBtn.setAttribute('aria-pressed', 'true');
+                    thisSlide.autoplay.stop();
+                } else if (autoPlayState === 'true') {
+                    autoPlayBtn.setAttribute('aria-pressed', 'false');
+                    thisSlide.autoplay.start();
+                };
+            });
+        },
+    },
 });
