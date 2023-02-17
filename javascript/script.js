2907acd4f1b41e2af460e0163769e48d8245a585
mulddang2 <96711699+mulddang2@users.noreply.github.com>
2023-02-17 20:30:05 +0900

    [mobile/tablet]
    - media query
    - btn-top 기능(스크롤 시, top버튼 생성 및 부드럽게 스크롤)
    - cast 레이아웃
    - scss, css 폴더경로 변경

--- javascript/script.js
+++ javascript/script.js
@@ -8,9 +8,25 @@ swiperNotice = new Swiper('.swiper.notice', {
         nextSlideMessage: '다음 슬라이드',
         slideLabelMessage: '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
     },
-});
+
+})
+
 swiperCast = new Swiper('.swiper.cast', {
     slidesPerView: 1,
+    centeredSlides: true,
+    breakpoints: {
+
+        768: {
+            slidesPerView: 2,  
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4,  
+            spaceBetween: 20,
+        },
+    },
+    loop: true,
+    loopedSlides: 1,
     autoplay: {
         delay: 1500,
     },
@@ -44,3 +60,28 @@ swiperCast = new Swiper('.swiper.cast', {
         },
     },
 });
+let Top = document.querySelector('.btn-top');
+Top.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+var rootElement = document.documentElement
+var showTopBtn = document.querySelector(".btn-top")
+var quickBuy = document.querySelector('.quick-buy')
+function handleScroll() {
+    // Do something on scroll
+    var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+    if ((rootElement.scrollTop / scrollTotal) > 0.20) {
+        // Show button
+        showTopBtn.classList.add("show-btn")
+        quickBuy.style.bottom = "19.1944444444vw";
+
+    } else {
+        // Hide button
+        showTopBtn.classList.remove("show-btn")
+        quickBuy.style.bottom = "3.125vw";
+
+    }
+}
+
+document.addEventListener("scroll", handleScroll)
